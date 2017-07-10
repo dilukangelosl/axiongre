5c58eb0a3bea1df66d19ff2edddc00830867942a
Diluk <dilukangelo@gmail.com>
2017-07-10 22:19:05 +0530

    s

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 var complete = false;
 
+var p = null;
+var PJS =   particlesJS.load("particles-js","js/p.json",function(){
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    p = window.pJSDom[0].pJS;
+    }
+})
+
+
+
+
+
 var myVivus = new Vivus('cic', {
   type: 'delayed',
   duration: 100,
@@ -12,12 +23,16 @@ $('body').mousedown(function(){
   //  $('.logocontainer').css({"width": "100%", "height": "100%","top": "0"});
 console.log("Mouse down");
 
+console.log(p);
+p.particles.number.value = 100;
 /*
     $( "video" ).fadeTo( "slow" , 1, function() {
     // Animation complete.
   });
   */
 
+
+console.log(PJS);
    if(!complete){
      myVivus.setFrameProgress(0);
    }
@@ -33,7 +48,8 @@ console.log("Mouse down");
 $('body').mouseup(function(){
 
 console.log("Mouse up");
-
+  p.particles.move.speed = 4;
+  p.particles.number.value = 22;
     if(!complete){
       myVivus.reset();
       myVivus.stop();
