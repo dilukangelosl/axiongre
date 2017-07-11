95c9b32c93235584c57a34c17f42fdb3aea01ff5
Diluk <dilukangelo@gmail.com>
2017-07-11 13:45:03 +0530

    h

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var complete = false;
-
+var cr = 100;
 var p = null;
 var PJS =   particlesJS.load("particles-js","js/p.json",function(){
   if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
@@ -11,19 +11,66 @@ var PJS =   particlesJS.load("particles-js","js/p.json",function(){
 
 
 
+var audio =  $('#audio');
+audio.trigger('play');
+var vid = document.getElementById("audio");
+vid.volume = 0.2;
+vid.playbackRate = 1.0;
+
+$('#playi').hide();
+
+$('#playi').click(function(){
+audio.trigger('play');
+$('#playi').hide();
+$('#pausei').show();
+})
+
+
+$('#pausei').click(function(){
+audio.trigger('pause');
+$('#playi').show();
+$('#pausei').hide();
+})
+
+var scene = document.getElementById('particles-js');
+var parallax = new Parallax(scene);
+new WOW().init();
+
 var myVivus = new Vivus('cic', {
   type: 'delayed',
-  duration: 100,
+  duration: 180,
   animTimingFunction: Vivus.EASE
 });
 myVivus.stop();
 myVivus.setFrameProgress(0);
 
+
+var increaseid = null;
+var decreaseid = null;
+
 $('body').mousedown(function(){
+
+  var vid = document.getElementById("audio");
+  vid.volume = 0.8;
+vid.playbackRate = 1.1;
+
   //  $('.logocontainer').css({"width": "100%", "height": "100%","top": "0"});
 console.log("Mouse down");
+//or customize things:
 
-console.log(p);
+if(decreaseid != null){
+  clearInterval(decreaseid);
+}
+increaseid = window.setInterval(
+     function () {
+        cr = cr + 5;
+         document.getElementById("mc").setAttribute("r", cr);
+
+     }, 100);
+
+
+
+p.particles.move.speed = 50;
 p.particles.number.value = 100;
 /*
     $( "video" ).fadeTo( "slow" , 1, function() {
@@ -35,6 +82,7 @@ p.particles.number.value = 100;
 console.log(PJS);
    if(!complete){
      myVivus.setFrameProgress(0);
+
    }
 
   myVivus.play(function() {
@@ -42,10 +90,33 @@ console.log(PJS);
     //$('.logocontainer').css({"width": "100%", "height": "100%","top": "0"});
   console.log("Done");
   complete = true;
+  $('#intro').css({"opacity":0})
+setInterval(function(){    window.location.href = 'home.html'; }, 1000);
+
+  vid.playbackRate = 1;
 })
 })
 
 $('body').mouseup(function(){
+
+  if(increaseid != null){
+  clearInterval(increaseid);
+  }
+
+
+  var vid = document.getElementById("audio");
+  vid.volume = 0.2;
+  vid.playbackRate = 0.8;
+
+  decreaseid = window.setInterval(
+       function () {
+          if(cr != 100){
+            cr = cr - 5;
+             document.getElementById("mc").setAttribute("r", cr);
+          }
+
+       }, 100);
+
 
 console.log("Mouse up");
   p.particles.move.speed = 4;
